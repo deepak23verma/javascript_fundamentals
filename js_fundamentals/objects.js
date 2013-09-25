78cf638aba8b0d687c47d48c76614d61cc3f2865
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-24 21:32:12 -0400

    complete books read array and iteration

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -15,7 +15,7 @@
 
 function ingredients(recipe) {
 	console.log(recipe.title);
-	console.log("Serves " + recipe.serves);
+	console.log("Serves: " + recipe.serves);
 	for (var i = 0; i < recipe.ingredients.length; i++) {
 		console.log(recipe.ingredients[i]);
 	}
@@ -37,6 +37,22 @@ ingredients(favoritRecipe);
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+var books = [{title: "The Alchamist", author: "Paulo Cohelo", alreadyRead: true}, {title: "Eloquent Ruby", author: "Russ Olsen", alreadyRead: false}, {title: "The Art of Happiness", author: "Dalai Lama", alreadyRead: true}];
+for (var i = 0; i < books.length; i++) {
+	console.log(books[i].title + " by " + books[i].author);
+}
+
+var books = [{title: "The Alchamist", author: "Paulo Cohelo", alreadyRead: true}, {title: "Eloquent Ruby", author: "Russ Olsen", alreadyRead: false}, {title: "The Art of Happiness", author: "Dalai Lama", alreadyRead: true}];
+for (var i = 0; i < books.length; i++) {
+	if (books[i].alreadyRead == true) {
+	console.log('I have already read "' + books[i].title + '" by ' + books[i].author);
+	}
+	else {
+		console.log('I have not yet read "' + books[i].title + '" by ' + books[i].author);
+	}
+};
+
+
 
 // EXERCISE: The Movie Database
 
