d2cd1e458c23372f245f8c32c540f6a4dc0282a4
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-24 22:10:09 -0400

    complete movie object and print function

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -60,6 +60,13 @@ for (var i = 0; i < books.length; i++) {
 
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+var movie = {title: "Oceans 11", duration: 116, stars: [" George Clooney", " Brad Pitt", " Matt Damon", " Don Ceadle"]}
 
+
+function printMovie (movie) {
+	console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + movie.stars)
+};
+
+printMovie(movie)
 
 
